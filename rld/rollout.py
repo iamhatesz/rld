ec176ff6d78d350c23b76992cb401f1ae64bf512
Tomasz Wrona <tomasz@wrona.me>
2020-07-11 22:43:42 +0200

    Improved tests for RayRollout

--- rld/rollout.py
+++ rld/rollout.py
@@ -5,7 +5,7 @@ from abc import ABC
 from collections import abc
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Iterator, List
+from typing import Iterator, List, Union
 
 import numpy as np
 
@@ -13,10 +13,26 @@ import numpy as np
 @dataclass
 class Timestep:
     obs: np.ndarray
-    action: np.ndarray
-    reward: np.ndarray
-    done: np.ndarray
+    action: Union[int, np.ndarray]
+    reward: float
+    done: bool
     info: dict
+
+    @staticmethod
+    def from_trajectory_index(
+        obs: np.ndarray,
+        action: Union[np.ndarray, np.ScalarType],
+        reward: np.ScalarType,
+        done: np.ScalarType,
+        info: dict,
+    ) -> Timestep:
+        return Timestep(
+            obs=obs,
+            action=int(action) if np.isscalar(action) else action,
+            reward=float(reward),
+            done=bool(done),
+            info=info,
+        )
 
 
 @dataclass
@@ -28,7 +44,7 @@ class Trajectory(abc.Iterable, abc.Sized):
     info: List[dict]
 
     def __getitem__(self, item: int) -> Timestep:
-        return Timestep(
+        return Timestep.from_trajectory_index(
             self.obs[item],
             self.action[item],
             self.reward[item],
@@ -94,7 +110,7 @@ class RayRollout(Rollout):
         return Trajectory(
             obs=np.stack([ts[0] for ts in trajectory], axis=0),
             action=np.stack([ts[1] for ts in trajectory], axis=0),
-            reward=np.stack([ts[3] for ts in trajectory], axis=0),
-            done=np.stack([ts[4] for ts in trajectory], axis=0),
+            reward=np.stack([float(ts[3]) for ts in trajectory], axis=0),
+            done=np.stack([bool(ts[4]) for ts in trajectory], axis=0),
             info=[ts[5] if len(ts) == 6 else {} for ts in trajectory],
         )
--- tests/test_rollout.py
+++ tests/test_rollout.py
@@ -1,6 +1,8 @@
 import unittest
 from pathlib import Path
 
+import numpy as np
+
 from rld.rollout import RayRollout
 
 
@@ -12,3 +14,16 @@ class TestRollout(unittest.TestCase):
         self.assertEqual(rollout.num_episodes, 5)
         for trajectory in rollout:
             self.assertGreater(len(trajectory), 0)
+
+            self.assertIsInstance(trajectory.obs, np.ndarray)
+            self.assertIsInstance(trajectory.action, np.ndarray)
+            self.assertIsInstance(trajectory.reward, np.ndarray)
+            self.assertIsInstance(trajectory.done, np.ndarray)
+            self.assertIsInstance(trajectory.info, list)
+
+            for timestep in trajectory:
+                self.assertIsInstance(timestep.obs, np.ndarray)
+                self.assertIsInstance(timestep.action, (int, np.ndarray))
+                self.assertIsInstance(timestep.reward, float)
+                self.assertIsInstance(timestep.done, bool)
+                self.assertIsInstance(timestep.info, dict)
