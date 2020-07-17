c431bd137df6c997dae59eb9e1581167fc69f361
Tomasz Wrona <tomasz@wrona.me>
2020-07-17 12:48:00 +0200

    Added flattening

--- rld/processors.py
+++ rld/processors.py
@@ -1,3 +1,4 @@
+from gym.spaces import unflatten, flatten
 from ray.rllib.models.modelv2 import restore_original_dimensions
 from ray.rllib.models.preprocessors import Preprocessor
 from ray.rllib.utils.torch_ops import convert_to_non_torch_type
@@ -39,7 +40,13 @@ class RayAttributionPostprocessor(AttributionPostprocessor):
         self.model = model
 
     def transform(self, attribution: AttributationLikeStrict) -> AttributationLike:
-        original = restore_original_dimensions(
+        attribution = restore_original_dimensions(
             attribution, self.model.obs_space, tensorlib="torch"
         )
-        return convert_to_non_torch_type(original)
+        attribution = convert_to_non_torch_type(attribution)
+        if hasattr(self.model.obs_space, "original_space"):
+            obs_space = self.model.obs_space.original_space
+        else:
+            obs_space = self.model.obs_space
+        attribution = unflatten(obs_space, flatten(obs_space, attribution))
+        return attribution
