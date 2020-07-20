b6320ad780b9b8b7b8242d5c48891eb9ae4106ea
Tomasz Wrona <tomasz@wrona.me>
2020-07-20 16:29:25 +0200

    To/from file rollout writer/reader

--- rld/tests/test_rollout.py
+++ rld/tests/test_rollout.py
@@ -2,12 +2,18 @@ import tempfile
 import unittest
 from pathlib import Path
 
+import numpy as np
 import ray
 from ray.rllib.agents.pg import PGTrainer
 from ray.rllib.agents.ppo import PPOTrainer
 from ray.rllib.rollout import rollout, RolloutSaver
 
-from rld.rollout import RayRolloutReader, FromMemoryRolloutReader
+from rld.rollout import (
+    RayRolloutReader,
+    FromMemoryRolloutReader,
+    ToFileRolloutWriter,
+    FromFileRolloutReader,
+)
 from rld.tests.resources.envs import collect_rollout, ALL_ENVS
 
 ALL_TRAINERS = [
@@ -23,14 +29,36 @@ class TestRolloutReader(unittest.TestCase):
     def tearDown(self) -> None:
         ray.shutdown()
 
-    def test_from_memory(self):
+    def test_memory_rollouter_reader(self):
         for env_fn in ALL_ENVS:
             with self.subTest(env=env_fn):
                 rollout = FromMemoryRolloutReader(collect_rollout(env_fn()))
                 for trajectory in rollout:
                     self.assertIsInstance(trajectory.timesteps, list)
 
-    def test_from_ray_rollout(self):
+    def test_file_rollout_reader_writer(self):
+        for env_fn in ALL_ENVS:
+            rollout = collect_rollout(env_fn())
+            with tempfile.TemporaryDirectory() as temp_dir:
+                rollout_file = Path(temp_dir) / "rollout"
+
+                with ToFileRolloutWriter(rollout_file) as writer:
+                    for trajectory in rollout.trajectories:
+                        writer.write(trajectory)
+
+                reader = FromFileRolloutReader(rollout_file)
+                for trajectory_l, trajectory_r in zip(rollout.trajectories, reader):
+                    for ts_l, ts_r in zip(trajectory_l, trajectory_r):
+                        np.testing.assert_equal(ts_l.obs, ts_r.obs)
+                        np.testing.assert_equal(ts_l.action, ts_r.action)
+                        self.assertEqual(ts_l.reward, ts_r.reward)
+                        self.assertEqual(ts_l.done, ts_r.done)
+                        np.testing.assert_equal(ts_l.info, ts_r.info)
+                        np.testing.assert_equal(
+                            ts_l.attributations, ts_r.attributations
+                        )
+
+    def test_ray_rollout_reader(self):
         for trainer_fn in ALL_TRAINERS:
             for env_fn in ALL_ENVS:
                 with self.subTest(trainer=trainer_fn, env=env_fn):
