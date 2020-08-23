de982dc123ec3599737025bbb34696011a89c173
Tomasz Wrona <tomasz@wrona.me>
2020-08-23 12:54:02 +0200

    Atari example in the form of Jupyter Notebook

--- examples/atari/collect_rollout.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-rllib rollout \
---run PPO \
---use-shelve \
---no-render \
---episodes 5 \
---out /tmp/atari-brakeout.ray_rollout \
-/tmp/ray-results/atari-breakout/PPO_BreakoutNoFrameskip-v4_0_2020-08-19_12-12-11eu5iymda/checkpoint_1/checkpoint-1
